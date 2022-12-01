81bc7176c2e5be90780cd57f1a9120da446a062b
rivea0 <58330360+rivea0@users.noreply.github.com>
2022-12-01 23:37:00 +0300

    Add README.md and day-01 challenge

---
+++ day-01/solution.py
@@ -0,0 +1,23 @@
+def parse(input_data):
+    with open(input_data) as f:
+        _id = 0
+        loads = {
+            _id: 0
+        }
+        for line in f.readlines():
+            if line == '\n':
+                _id += 1
+            if line.strip('\n').isdigit():
+                loads[_id] = loads.get(_id, 0) + int(line)
+
+    return loads 
+
+
+def part1():
+    loads = parse('input.txt')
+    return max(loads.values())
+
+
+def part2():
+    loads = parse('input.txt')
+    return sum(sorted(loads.values(), reverse=True)[:3])
